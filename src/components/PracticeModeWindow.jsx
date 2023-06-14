42d38dd46d64b213c2079e0cd284b314061c97c2
Garyk <garyk.simpson@proton.me>
2023-06-14 16:22:28 -0600

    get final score to work properly in practice mode

--- src/components/PracticeModeWindow.jsx
+++ src/components/PracticeModeWindow.jsx
@@ -6,35 +6,44 @@ export function PracticeModeWindow(props) {
   const [index, setIndex] = useState(0);
   const [passScore, setPassScore] = useState(0);
   const [failScore, setFailScore] = useState(0);
+  const [finalScore, setFinalScore] = useState(0);
 
   function pass() {
-    if (index < props.deck.length) {
+    if (index < props.deck.length - 1) {
       setIndex((old) => old + 1);
       setPassScore((old) => old + 1);
     } else {
-      props.changePracticeMode();
+      setFinalScore((passScore / props.deck.length) * 100);
     }
   }
 
+  // How to get score to the main file?
+
   function fail() {
-    if (index < props.deck.length) {
+    if (index < props.deck.length - 1) {
       setIndex((old) => old + 1);
       setFailScore((old) => old + 1);
     } else {
-      props.changePracticeMode();
+      setFinalScore((passScore / props.deck.length) * 100);
     }
+  }
+
+  function endPracticeMode() {
+    props.changePracticeMode();
   }
 
   return (
     <div id="practice-mode-window">
-      <div id="new-card-onclick" onClick={() => setShowButtons(true)}>
-        <Card
-          card={props.deck[index]}
-          flip={props.flip}
-          flipCard={props.flipCard}
-          practiceMode={props.practiceMode}
-        />
-      </div>
+      {finalScore ? null : (
+        <div id="new-card-onclick" onClick={() => setShowButtons(true)}>
+          <Card
+            card={props.deck[index]}
+            flip={props.flip}
+            flipCard={props.flipCard}
+            practiceMode={props.practiceMode}
+          />
+        </div>
+      )}
 
       {showButtons ? (
         <div id="btn--pass-fail">
@@ -62,9 +71,14 @@ export function PracticeModeWindow(props) {
           </button>
         </div>
       ) : null}
-      <h6>
-        Passed: {passScore} <br></br>Failed: {failScore}
-      </h6>
+
+      {finalScore ? (
+        <h6>Final Score: {finalScore}</h6>
+      ) : (
+        <h6>
+          Passed: {passScore} <br></br>Failed: {failScore}
+        </h6>
+      )}
     </div>
   );
 }
